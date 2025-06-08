f5e038e9f81c7f1819e450ddd76bdab1fcaf431f
Sounak Patra <105234492+SounakPatra7@users.noreply.github.com>
2025-06-08 19:48:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import predictionRoutes from './routes/predictionRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
