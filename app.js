fdc66ba5e57b56b4a6373c607247ede0a8a316a7
Sounak Patra <105234492+SounakPatra7@users.noreply.github.com>
2025-06-08 19:11:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ import predictionRoutes from './routes/predictionRoutes.js';
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    'http://localhost:3000', 
-    'https://tradexai.netlify.app/'
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
