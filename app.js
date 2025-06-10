159a95035d02760e970d09a9b150243b4c663681
Sounak Patra <105234492+SounakPatra7@users.noreply.github.com>
2025-06-10 19:32:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,18 +8,7 @@ import predictionRoutes from './routes/predictionRoutes.js';
 
 const app = express();
 
-const allowedOrigins = ['https://effortless-dieffenbachia-514251.netlify.app'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
